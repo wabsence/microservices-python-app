e49127c1d3853ceeb7585d0d9de64657d89cee38
wabsense <wahabfactorial@gmail.com>
2025-05-26 20:48:50 +0100

    Update commit

--- src/gateway-service/server.py
+++ src/gateway-service/server.py
@@ -6,23 +6,22 @@ from auth_svc import access
 from storage import util
 from bson.objectid import ObjectId
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from prometheus_client import Counter
+
+# Define Prometheus metrics
+unauth_count = Counter('unauthorized_requests_total', 'Total unauthorized requests')
 
 server = Flask(__name__)
-
 mongo_video = PyMongo(server, uri=os.environ.get('MONGODB_VIDEOS_URI'))
-
 mongo_mp3 = PyMongo(server, uri=os.environ.get('MONGODB_MP3S_URI'))
-
 fs_videos = gridfs.GridFS(mongo_video.db)
 fs_mp3s = gridfs.GridFS(mongo_mp3.db)
-
 connection = pika.BlockingConnection(pika.ConnectionParameters(host="rabbitmq", heartbeat=0))
 channel = connection.channel()
 
 @server.route("/login", methods=["POST"])
 def login():
     token, err = access.login(request)
-
     if not err:
         return token
     else:
@@ -31,23 +30,17 @@ def login():
 @server.route("/upload", methods=["POST"])
 def upload():
     access, err = validate.token(request)
-
     if err:
         unauth_count.inc()
         return err
-
     access = json.loads(access)
-
     if access["admin"]:
         if len(request.files) > 1 or len(request.files) < 1:
             return "exactly 1 file required", 400
-
         for _, f in request.files.items():
             err = util.upload(f, fs_videos, channel, access)
-
             if err:
                 return err
-
         return "success!", 200
     else:
         return "not authorized", 401
@@ -55,28 +48,21 @@ def upload():
 @server.route("/download", methods=["GET"])
 def download():
     access, err = validate.token(request)
-
     if err:
         unauth_count.inc()
         return err
-
     access = json.loads(access)
-
     if access["admin"]:
         fid_string = request.args.get("fid")
-
         if not fid_string:
             return "fid is required", 400
-
         try:
             out = fs_mp3s.get(ObjectId(fid_string))
             return send_file(out, download_name=f"{fid_string}.mp3")
         except Exception as err:
             print(err)
             return "internal server error", 500
-
     return "not authorized", 401
-
 
 if __name__ == "__main__":
     server.run(host="0.0.0.0", port=8080)
